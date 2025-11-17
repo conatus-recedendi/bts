362f0b73a29ae9b3d435abda0e914e309ae0b84e
kyle <kyle@wrtn.io>
2025-11-17 17:13:44 +0900

    fix

---
+++ run/parse_task.sh
@@ -0,0 +1,9 @@
+../dataset/python decompose_letters.py WS353_korean.csv WS353_korean_default.csv False False
+../dataset/python decompose_letters.py WS353_korean.csv WS353_korean_stroke.csv True False
+../dataset/python decompose_letters.py WS353_korean.csv WS353_korean_cji.csv False True
+../dataset/python decompose_letters.py WS353_korean.csv WS353_korean_bts.csv True True
+
+../dataset/python decompose_letters.py word_analogy_korean.txt word_analogy_korean_default.txt False False
+../dataset/python decompose_letters.py word_analogy_korean.txt word_analogy_korean_stroke.csv True False
+../dataset/python decompose_letters.py word_analogy_korean.txt word_analogy_korean_cji.csv False True
+../dataset/python decompose_letters.py word_analogy_korean.txt word_analogy_korean_bts.csv True True
